2eff159503f2c8b3399e88b4f952d1de8e3c0c1c
senthilksp <senthilkumar.sp@madronesoft.com>
2014-01-06 16:32:03 +0530

    LQ : Bean file changes are checked in.

---
+++ plugins/liferay-plugins-sdk-6.2.0/portlets/LQPortlet-portlet/docroot/WEB-INF/src/com/cti/lq/beans/QuestTransactionBean.java
@@ -0,0 +1,29 @@
+package com.cti.lq.beans;
+
+public class QuestTransactionBean {
+
+	private int QuestId;
+	private String QuestType;
+	private String QuestLocation;
+	
+	public int getQuestId() {
+		return QuestId;
+	}
+	public void setQuestId(int questId) {
+		QuestId = questId;
+	}
+	public String getQuestType() {
+		return QuestType;
+	}
+	public void setQuestType(String questType) {
+		QuestType = questType;
+	}
+	public String getQuestLocation() {
+		return QuestLocation;
+	}
+	public void setQuestLocation(String questLocation) {
+		QuestLocation = questLocation;
+	}
+	
+
+}
